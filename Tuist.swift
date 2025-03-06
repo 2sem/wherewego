65482e05667db580631c74ee65acacfff0ba99e5
leesam <toyboy2@hanmail.net>
2025-03-06 20:50:52 +0900

    refactor: upgrade tuist to 4.38.2

---
+++ Tuist.swift
@@ -0,0 +1,17 @@
+//
+//  Tuist.swift
+//  wherewegoManifests
+//
+//  Created by 영준 이 on 3/6/25.
+//
+
+import ProjectDescription
+
+let tuist = Tuist(
+    project: .tuist(compatibleXcodeVersions: .upToNextMajor("16.0")
+//                    swiftVersion: "",
+//                    plugins: <#T##[PluginLocation]#>,
+//                    generationOptions: <#T##Tuist.GenerationOptions#>,
+//                    installOptions: <#T##Tuist.InstallOptions#>)
+    )
+)
